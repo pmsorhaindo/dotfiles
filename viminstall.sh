c3f3ab67c5de91978248e4bfcc9ff4226a329217
Michael Sorhaindo <pmsorhaindo@gmail.com>
2015-12-18 17:45:47 +0000

    vim install script

---
+++ viminstall.sh
@@ -0,0 +1,4 @@
+make distclean && 
+    ./configure --prefix ~/***** SOME DIR ***/Work/vim-built --with-features=huge --enable-perlinterp --enable-pythoninterp --enable-rubyinterp --enable-multibyte --enable-fontset && 
+        make && 
+            make install
